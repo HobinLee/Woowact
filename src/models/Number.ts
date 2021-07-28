5c6e46a99ad0047879fe343f88ccf75de06cd167
Hov Lee <35447853+HobinLee@users.noreply.github.com>
2021-07-29 00:46:11 +0900

    refactor: dispatch를 이용하여 데이터 변경

--- src/models/Number.ts
+++ src/models/Number.ts
@@ -1,13 +1,35 @@
 import { Store } from '../lib/woowact/core/Store';
 
-type NumberStore = {
+export const CHANGE_NUMBER = 'Number/CHANGE' as const;
+
+type NumberData = {
   pickedNumber: number;
 };
 
-class Number extends Store<NumberStore> {
-  constructor(numberStore: NumberStore) {
+class NumberStore extends Store<NumberData> {
+  constructor(numberStore: NumberData) {
     super(numberStore);
   }
+
+  actions = {
+    [CHANGE_NUMBER]: this.changeCurrentNumber,
+  };
+
+  changeCurrentNumber(number: number): NumberData {
+    return {
+      pickedNumber: number,
+    };
+  }
+
+  updateStore = (action: string, args: Partial<NumberData>) => {
+    switch (action) {
+      case CHANGE_NUMBER:
+        this.updateData(args);
+        break;
+      default:
+        break;
+    }
+  };
 }
 
-export const numberStore: Number = new Number({ pickedNumber: 0 });
+export const numberStore: NumberStore = new NumberStore({ pickedNumber: 0 });
