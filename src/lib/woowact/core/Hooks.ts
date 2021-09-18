ce5884657c84ecd436dfd0f6a7413b86fd7b093e
HobinLee <viny5120@gmail.com>
2021-09-18 13:12:11 +0900

    feat: Hook 개발중

---
+++ src/lib/woowact/core/Hooks.ts
@@ -0,0 +1,27 @@
+import { WoowactNode } from "./VDOM";
+
+export const Woowact = (function(){
+  let hooks: any[] = [];
+  let idx = 0;
+
+  function useState<T> (initVaule: T): [T, (newValue: T) => void] {
+    const state: T = hooks[idx] || initVaule;
+
+    const _idx = idx;
+  
+    function setState(newValue: T) {
+      hooks[_idx] = newValue;
+    }
+
+    idx ++;
+    
+    return [state, setState];
+  }
+
+  function render(node: WoowactNode) {
+    idx = 0;
+    return node;
+  }
+
+  return {useState, render}
+}())
