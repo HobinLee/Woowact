b58673a44e757e999eb7dc053e4af8ea34315c1f
HobinLee <viny5120@gmail.com>
2021-09-19 15:32:21 +0900

    feat: Hook 개발중 (App에 적용)

--- src/lib/woowact/app/App.ts
+++ src/lib/woowact/app/App.ts
@@ -1,19 +1,32 @@
-import { ComponentA } from '../../../components/ComponentA';
-import { ComponentB } from '../../../components/ComponentB';
-import { createElement, WoowactNode } from '../core/VDOM';
+import { Woowact } from '../core/Hooks';
+import { WoowactElement } from '../core/VDOM';
 
-export const App = (): string => {
-  let count = 20;
+export const App = (): WoowactElement => {
+  const [count, setCount] = Woowact.useState<number>(0);
+  const [age, setAge] = Woowact.useState<number>(100);
+
   const handleClick = () => {
-    alert(`count is ${count ++}`);
+    setCount(count + 1);
+  }
+
+  const handleAgeClick = () => {
+    setAge(age + 1);
   }
   
-  return createElement(`<div>
-    <h3>Hello! this is test page</h3>
-    <button onclick="aa">+</button>
-    ${ComponentA('testA')}
-    ${ComponentA('testB')}
-
-    ${ComponentB(count)}
-  </div>`);
+  return {
+    tag: 'div',
+    children: [{
+        tag: 'h3',
+        children: [count.toString()]
+      }, {
+        tag: 'button',
+        children: ['+'],
+        attributes: new Map().set('onclick', handleClick)
+      }, {
+        tag: 'button',
+        children: ['++'],
+        attributes: new Map().set('onclick', handleAgeClick)
+      }
+    ]
+  }
 }
