67284b7147c037c9180c76a7b329e826be1b5bb2
HobinLee <viny5120@gmail.com>
2021-09-22 18:02:27 +0900

    feat: 컴포넌트화 시키기

--- src/components/ComponentA.ts
+++ src/components/ComponentA.ts
@@ -1,7 +1,23 @@
-import { createElement, WoowactNode } from '../lib/woowact/core/VDOM';
+import { Woowact } from '../lib/woowact/core/Hooks';
+import { WoowactNode } from '../lib/woowact/core/VDOM';
 
-export const ComponentA = (text: string): WoowactNode => {
-  return createElement(`<div>
-    <h5>This is Component A ${text}</h5>
-  </div>`);
+export const ComponentA = (initValue: number): WoowactNode => {
+  const [count, setCount] = Woowact.useState<number>(initValue);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  }
+  
+  return {
+    tag: 'div',
+    children: [{
+        tag: 'h3',
+        children: [count.toString()]
+      }, {
+        tag: 'button',
+        children: ['+'],
+        attributes: new Map().set('onclick', handleClick)
+      }
+    ]
+  }
 }
--- src/lib/woowact/app/App.ts
+++ src/lib/woowact/app/App.ts
@@ -1,32 +1,12 @@
-import { Woowact } from '../core/Hooks';
+import { ComponentA } from '../../../components/ComponentA';
 import { WoowactElement } from '../core/VDOM';
 
 export const App = (): WoowactElement => {
-  const [count, setCount] = Woowact.useState<number>(0);
-  const [age, setAge] = Woowact.useState<number>(100);
-
-  const handleClick = () => {
-    setCount(count + 1);
-  }
-
-  const handleAgeClick = () => {
-    setAge(age + 1);
-  }
-  
   return {
     tag: 'div',
-    children: [{
-        tag: 'h3',
-        children: [count.toString()]
-      }, {
-        tag: 'button',
-        children: ['+'],
-        attributes: new Map().set('onclick', handleClick)
-      }, {
-        tag: 'button',
-        children: ['++'],
-        attributes: new Map().set('onclick', handleAgeClick)
-      }
+    children: [
+      ComponentA(0),
+      ComponentA(100)
     ]
   }
 }
